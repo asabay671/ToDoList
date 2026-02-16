90bd358ae4e3a2b0639d1ffa5d5fa4dabacdb092
asabay671 <foggert234@gmail.com>
2026-02-16 17:21:56 +0500

    pipes and components

---
+++ src/app/components/dialog-icon/dialog-icon-component.ts
@@ -0,0 +1,31 @@
+import { ChangeDetectionStrategy, Component, ViewChild, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DialogAnimationsDialogComponent } from '../dialog-animation-dialog/dialog-animation-dialog.component';
+import { CardMediaSizeExample } from '../card-media-size-example/card-media-size.component';
+
+@Component({
+  selector: 'app-dialog-animations',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogModule, CardMediaSizeExample],
+  templateUrl: './dialog-icon.component.html',
+  styleUrl: 'dialog-icon.component.css',
+
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAnimationsExample {
+  @ViewChild(CardMediaSizeExample)
+  private boardComponent!: CardMediaSizeExample;
+
+  private dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAnimationsDialogComponent);
+
+    dialogRef.afterClosed().subscribe((title?: string) => {
+      if (!title) return;
+
+      this.boardComponent.addTaskToNew(title);
+    });
+  }
+}
